e790f1fad80091bc7f6248c3ff69ec661e076fed
Andrew Foresi <foresiandrew@gmail.com>
2017-06-17 18:21:15 -0700

    broken tests

--- dist/built.js
+++ dist/built.js
@@ -38,7 +38,8 @@ window.Shortly = Backbone.View.extend({
       .addClass('selected');
   }
 });
-;Shortly.createLinkView = Backbone.View.extend({
+
+Shortly.createLinkView = Backbone.View.extend({
   className: 'creator',
 
   template: Templates.create,
@@ -93,10 +94,12 @@ window.Shortly = Backbone.View.extend({
       .removeClass('error');
   }
 });
-;Shortly.Link = Backbone.Model.extend({
+
+Shortly.Link = Backbone.Model.extend({
   urlRoot: '/links'
 });
-;Shortly.LinkView = Backbone.View.extend({
+
+Shortly.LinkView = Backbone.View.extend({
   className: 'link',
 
   template: Templates.link,
@@ -107,11 +110,13 @@ window.Shortly = Backbone.View.extend({
     return this;
   }
 });
-;Shortly.Links = Backbone.Collection.extend({
+
+Shortly.Links = Backbone.Collection.extend({
   model: Shortly.Link,
   url: '/links'
 });
-;Shortly.LinksView = Backbone.View.extend({
+
+Shortly.LinksView = Backbone.View.extend({
   className: 'links',
 
   initialize: function() {
@@ -133,7 +138,8 @@ window.Shortly = Backbone.View.extend({
     this.$el.append(view.render().el);
   }
 });
-;Shortly.Router = Backbone.Router.extend({
+
+Shortly.Router = Backbone.Router.extend({
   initialize: function(options) {
     this.$el = options.el;
   },
